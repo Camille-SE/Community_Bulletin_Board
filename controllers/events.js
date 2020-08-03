5981adef9a3c89d12f6ae669c2c7ff5035903695
slowkuruma <stevan.han82@gmail.com>
2020-08-03 09:18:42 -0700

    future events

--- controllers/events.js
+++ controllers/events.js
@@ -5,64 +5,64 @@ const Future = require('../models/futureEvents')
 
 // ---Index
 router.get('/', async (req, res) => {
-    try{
-        const events = await Today.find({});
-        res.render('events/index.ejs', {
-            foundEvents : events,
-        });
-    } catch (error) {
-        res.send(error)
-    }
+  try {
+    const events = await Today.find({});
+    res.render('events/index.ejs', {
+      foundEvents: events,
+    });
+  } catch (error) {
+    res.send(error)
+  }
 });
 
 //-----Get New Event
 router.get('/new', (req, res) => {
-    res.render('events/new.ejs');
-  });
+  res.render('events/new.ejs');
+});
 
 //-----Get Events Show
 router.get('/:id', async (req, res) => {
-    try {
-      const event = await Today.findById(req.params.id);
-      res.render('events/show.ejs', {
-        foundEvent: event,
-      });
-    } catch (error) {
-      res.send(err);
-    }
-  });
+  try {
+    const event = await Today.findById(req.params.id);
+    res.render('events/show.ejs', {
+      foundEvent: event,
+    });
+  } catch (error) {
+    res.send(err);
+  }
+});
 
 //-----Get Event Edit
 router.get('/:id/edit', async (req, res) => {
-    try {
-      const event = await Today.findById(req.params.id);
-      res.render('events/edit.ejs', {
-        foundEvent: event,
-      });
-    } catch (error) {
-      res.send(err);
-    }
-  });
-  
+  try {
+    const event = await Today.findById(req.params.id);
+    res.render('events/edit.ejs', {
+      foundEvent: event,
+    });
+  } catch (error) {
+    res.send(err);
+  }
+});
+
 //-----Get Events Update
 router.put('/:id', async (req, res) => {
-    try {
-      await Today.findByIdAndUpdate(req.params.id, req.body, {new: true});
-      res.redirect('/events');
-    } catch (error) {
-      res.send(err);
-    }
-  });
+  try {
+    await Today.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.redirect('/events');
+  } catch (error) {
+    res.send(err);
+  }
+});
 
 //----Get Events Create
 router.post('/', async (req, res) => {
-    console.log(req.body);
-    try {
-      await Today.create(req.body);
-      res.redirect('/events');
-    } catch (error) {
-      res.send(err);
-    }
-  });
-  
+  console.log(req.body);
+  try {
+    await Today.create(req.body);
+    res.redirect('/events');
+  } catch (error) {
+    res.send(err);
+  }
+});
+
 module.exports = router;
--- controllers/futureEvents.js
+++ controllers/futureEvents.js
@@ -4,6 +4,66 @@ const Today = require('../models/events')
 const Future = require('../models/futureEvents')
 
 
+router.get('/', async (req, res) => {
+    try {
+        const events = await FutureEvents.find({});
+        res.render('events/index.ejs', {
+            foundFutureEvents: futureEvents,
+        });
+    } catch (error) {
+        res.send(error)
+    }
+});
+
+//-----Get New Event
+router.get('/new', (req, res) => {
+    res.render('futureEvents/new.ejs');
+});
+
+//-----Get Events Show
+router.get('/:id', async (req, res) => {
+    try {
+        const event = await FutureEvents.findById(req.params.id);
+        res.render('futureEvents/show.ejs', {
+            foundFutureEvent: futureEvent,
+        });
+    } catch (error) {
+        res.send(err);
+    }
+});
+
+//-----Get Event Edit
+router.get('/:id/edit', async (req, res) => {
+    try {
+        const event = await FutureEvents.findById(req.params.id);
+        res.render('futureEvents/edit.ejs', {
+            foundFutureEvent: futureEvent,
+        });
+    } catch (error) {
+        res.send(err);
+    }
+});
+
+//-----Get Events Update
+router.put('/:id', async (req, res) => {
+    try {
+        await Future.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        res.redirect('/futureEvents');
+    } catch (error) {
+        res.send(err);
+    }
+});
+
+//----Get Events Create
+router.post('/', async (req, res) => {
+    console.log(req.body);
+    try {
+        await Future.create(req.body);
+        res.redirect('/futureEvents');
+    } catch (error) {
+        res.send(err);
+    }
+});
 
 
 
